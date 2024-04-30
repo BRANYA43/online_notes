52c8bfe6a202999d254f17aae10edcc11e714461
BRANYA <tbd2504@gmail.com>
2024-04-30 17:34:43 +0300

    Create User model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,1 +1,30 @@
-# Create your models here.
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+from django.utils.translation import gettext as _
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        verbose_name=_('email'),
+        unique=True,
+    )
+    last_login = models.DateTimeField(
+        verbose_name=_('last login'),
+        auto_now=True,
+    )
+    created = models.DateTimeField(
+        verbose_name=_('created'),
+        auto_now_add=True,
+    )
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        ordering = ['-created']
+
+    def __str__(self):
+        return str(self.email)
---
+++ src/accounts/tests/test_models.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from django.test import TestCase
+
+from accounts import models
+
+
+class UserModelTest(TestCase):
+    def setUp(self) -> None:
+        self.model_class = models.User
+        self.email = 'rick.sanchez@test.com'
+        self.password = 'qwe123!@#'
+        self.data = {
+            'email': self.email,
+            'password': self.password,
+        }
+
+    def test_model_inherit_necessary_mixins(self):
+        mixins = [PermissionsMixin, AbstractBaseUser]
+        [self.assertTrue(issubclass(self.model_class, mixin)) for mixin in mixins]
+
+    def test_email_field_is_required(self):
+        with self.assertRaises(ValidationError):
+            user = self.model_class.objects.create(password=self.password)
+            user.full_clean()
+
+    def test_email_field_is_unique(self):
+        self.model_class.objects.create(**self.data)
+        self.assertRaises(IntegrityError, self.model_class.objects.create, **self.data)
+
+    def test_email_field_is_username_field(self):
+        self.assertEqual(self.model_class.USERNAME_FIELD, 'email')
+
+    def test_email_field_is_email_field(self):
+        self.assertEqual(self.model_class.EMAIL_FIELD, 'email')
+
+    def test_model_str_representation_is_email(self):
+        user = self.model_class(**self.data)
+        self.assertEqual(str(user), self.email)
