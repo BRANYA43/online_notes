f2bfabffff3fdb8846507f87c173cd66a9f6f5ed
BRANYA <tbd2504@gmail.com>
2024-04-30 13:39:44 +0300

    Create general static for web

--- src/core/settings/components/base.py
+++ src/core/settings/components/base.py
@@ -80,7 +80,8 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
