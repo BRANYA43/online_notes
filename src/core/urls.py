3aa65c6401a38d785e41b208708e1507abd12ecc
BRANYA <tbd2504@gmail.com>
2024-04-30 13:34:59 +0300

    Create Accounts app

--- src/core/urls.py
+++ src/core/urls.py
@@ -3,9 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+urlpatterns = [path('admin/', admin.site.urls), path('account/', include('accounts.urls'))]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
