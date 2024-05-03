d4af935573dfb7fe0d5ffaafe737b0ae48db6cb8
BRANYA <tbd2504@gmail.com>
2024-05-03 14:40:57 +0300

    Add Worktable to admin

--- src/notes/admin.py
+++ src/notes/admin.py
@@ -1,1 +1,14 @@
-# Register your models here.
+from django.contrib import admin
+
+from notes import models
+
+
+@admin.register(models.Worktable)
+class WorktableAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+    fieldsets = (('Information', {'fields': ('user',)}),)
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ('user',)
+        return self.readonly_fields
