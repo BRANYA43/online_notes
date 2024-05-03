91f303e693a17320138ae29d42c75c98d2941934
BRANYA <tbd2504@gmail.com>
2024-05-03 14:45:26 +0300

    Add Category to admin

--- src/notes/admin.py
+++ src/notes/admin.py
@@ -3,6 +3,17 @@ from django.contrib import admin
 from notes import models
 
 
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'color', 'worktable')
+    fieldsets = (('Information', {'fields': ('worktable', 'title', 'color')}),)
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ('worktable',)
+        return self.readonly_fields
+
+
 @admin.register(models.Worktable)
 class WorktableAdmin(admin.ModelAdmin):
     list_display = ('user',)
