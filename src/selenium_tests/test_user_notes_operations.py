9847f8fd0f89a75843a6d99cde7942a9c5392baa
BRANYA <tbd2504@gmail.com>
2024-05-06 16:30:04 +0300

    Add selenium tests of archiving note for users

--- src/selenium_tests/test_user_notes_operations.py
+++ src/selenium_tests/test_user_notes_operations.py
@@ -214,7 +214,7 @@ class RegisteredUserNotesOperationsTest(FunctionalTestCase):
         # User logins to site
         self.login_user_through_selenium()
 
-        # User sees a note in the note list and click on edit button
+        # User sees a note in the note list and click on delete button
         card = self.wait_for(
             lambda: self.browser.find_element(value='note_list').find_element(By.CLASS_NAME, 'card'),
         )
@@ -223,6 +223,30 @@ class RegisteredUserNotesOperationsTest(FunctionalTestCase):
             note_title=note.title,
         )
         card.find_element(value='delete').click()
+
+        # User sees empty note list
+        self.wait_for(
+            lambda: self.browser.find_element(value='note_list').find_elements(By.CLASS_NAME, 'card'),
+            expected_value=[],
+        )
+
+    def test_user_can_archive_choice_note_from_note_list(self):
+        note = Note.objects.create(worktable=self.worktable, title='Note #1', text='Some Text')
+        # User enters to site
+        self.enter_to_site()
+
+        # User logins to site
+        self.login_user_through_selenium()
+
+        # User sees a note in the note list and click on archive button
+        card = self.wait_for(
+            lambda: self.browser.find_element(value='note_list').find_element(By.CLASS_NAME, 'card'),
+        )
+        self.check_note_value_in_the_card(
+            card,
+            note_title=note.title,
+        )
+        card.find_element(value='archive').click()
 
         # User sees empty note list
         self.wait_for(
@@ -430,7 +454,7 @@ class AnonymousUserNotesOperationsTest(FunctionalTestCase):
         # User enters to site
         self.enter_to_site()
 
-        # User sees a note in the note list and click on edit button
+        # User sees a note in the note list and click on delete button
         card = self.wait_for(
             lambda: self.browser.find_element(value='note_list').find_element(By.CLASS_NAME, 'card'),
         )
@@ -439,6 +463,28 @@ class AnonymousUserNotesOperationsTest(FunctionalTestCase):
             note_title=note.title,
         )
         card.find_element(value='delete').click()
+
+        # User sees empty note list
+        self.wait_for(
+            lambda: self.browser.find_element(value='note_list').find_elements(By.CLASS_NAME, 'card'),
+            expected_value=[],
+        )
+
+    def test_user_can_archive_choice_note_from_note_list(self):
+        note = Note.objects.create(worktable=self.get_worktable(), title='Note #1', text='Some Text')
+
+        # User enters to site
+        self.enter_to_site()
+
+        # User sees a note in the note list and click on archive button
+        card = self.wait_for(
+            lambda: self.browser.find_element(value='note_list').find_element(By.CLASS_NAME, 'card'),
+        )
+        self.check_note_value_in_the_card(
+            card,
+            note_title=note.title,
+        )
+        card.find_element(value='archive').click()
 
         # User sees empty note list
         self.wait_for(
