ca49ca25e624e7be75d09ae55163f1cdd3bbc5a6
BRANYA <tbd2504@gmail.com>
2024-05-06 16:29:29 +0300

    Add ajax to archiving note

--- src/static/js/base.js
+++ src/static/js/base.js
@@ -45,6 +45,27 @@ $(document).ready(function(){
         `);
     }
 
+    $('#note_list').on('click', '#archive', function(event) {
+        event.preventDefault();
+
+        send_ajax_request(
+            data={},
+            type='GET',
+            url=$(this).attr('href'),
+            success=function(response) {
+                var form = $('form#note_form');
+                if(form.attr('action').includes('update') && form.attr('action').includes(`${response.note.id}`)){
+                    form.trigger('reset');
+                    form.attr('action', form.attr('data-create-url'));
+                }
+                $(`#${response.note.id}`).remove();
+            },
+            error=function(xhr, status, error){
+                console.error(error);
+
+        });
+    });
+
     $('#note_list').on('click', '#delete', function (event){
         event.preventDefault();
 
