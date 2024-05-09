bfa0c592834dc508d85a64aa54d56286b9048ee8
BRANYA <tbd2504@gmail.com>
2024-05-09 17:27:17 +0300

    Add django baton admin

--- src/core/settings/__init__.py
+++ src/core/settings/__init__.py
@@ -8,6 +8,7 @@ from core.settings.components import env
 
 _settings = (
     'components/base.py',
+    'components/baton.py',
     'components/{}.py'.format(env.get('DJANGO_SETTINGS_ENV', 'prod').lower()),
 )
 
